844bf1954e51a0745240e77b69a8ed6f5875ee28
葛晓康 <1525185228@qq.com>
2021-02-11 20:01:37 +0800

    ....

--- src/app/index.js
+++ src/app/index.js
@@ -12,11 +12,16 @@ const app = new Koa();
 //应用koa-bodyparser这个中间件的客户端的json数据进行解析
 app.use(bodyParser())
 
-//调用循环注册路由的函数，将app传入
+//利用node的文件读写模块，循环注册路由的函数，将app传入,重点
 useRoutesApp(app)
 
 //响应错误，并处理
 app.on('error', errorHandler)
+
+//监听端口
+app.listen(APP_PORT, () => {
+  console.log(APP_PORT + '端口监听成功')
+})
 
 //导出app
 module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
 //导入app
-const app = require('./app')
-const connection = require('./app/database')
-//导入环境变量的数据
-const { APP_PORT } = require('./app/config.js')
-
-//监听端口
-app.listen(APP_PORT, () => {
-  console.log(APP_PORT + '端口监听成功')
-})
+require('./app')
